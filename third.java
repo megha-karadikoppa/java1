af310c1f9ea88ef363b580a97ea49a82c786b5c9
meghakaradikoppa <meghakaradikoppa@gmail.com>
2025-04-04 09:36:04 +0530

    save file

---
+++ third.java
@@ -0,0 +1,17 @@
+class Animal {
+    void eat() {
+        System.out.println("This animal eats food.");
+    }
+}
+class Dog extends Animal {
+    void bark() {
+        System.out.println("The dog barks.");
+    }
+}
+public class InheritanceExample {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.eat();
+        myDog.bark();
+    }
+}
